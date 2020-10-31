b26e5b2d3226071ffc8e062debc961caf0dbb0c5
raihanbimaac <raihanbimaac@gmail.com>
2020-10-31 14:44:26 +0700

    Create praktikum3.py

---
+++ praktikum3.py
@@ -0,0 +1,19 @@
+#nomer5
+from Operator import *
+#a
+a= kali(4,6)
+b= jumlah(2,a)
+c= kurang(b,4)
+print(c)
+#b
+m=jumlah(4,7)
+n=kurang(6,9)
+o=kali(m,n)
+print(o)
+#c
+p=jumlah(10,2)
+q=jumlah(7,5)
+r=kurang(12,34)
+s=bagi(p,q)
+t=bagi(s,r)
+print(t)
