b18ea9bd952c50fe5cb58af0df2e489b73b965f4
raihanbimaac <raihanbimaac@gmail.com>
2020-10-31 14:44:21 +0700

    Create Praktikum2.py

---
+++ Praktikum2.py
@@ -0,0 +1,29 @@
+#nomer1
+def luasSegitiga(alas,tinggi):
+    luas = alas*tinggi/2
+    return luas
+
+alas=10
+tinggi=20
+print('Luas segitigas dengan alas', alas,
+      'dan tinggi', tinggi,
+      'adalah', luasSegitiga(alas,tinggi))
+#nomer6
+alas2 = 15
+tinggi2 = 45
+print('Luas segitiga dengan alas ', alas2,
+        ' dan tinggi ', tinggi2,
+        ' adalah ', luasSegitiga(alas2,tinggi2))
+#nomer3
+def luasSegitiga2(alas,tinggi):
+    luas = alas*tinggi/2
+    print('Luas segitigas dengan alas', alas,
+      'dan tinggi', tinggi,
+      'adalah', luasSegitiga(alas,tinggi))
+alas=10
+tinggi=20
+luasSegitiga2(alas,tinggi)
+#nomer7
+a2 = 15
+t2= 45
+luasSegitiga2(a2,t2)
