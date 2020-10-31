b1e970e5ae98a18d9bc09fbc1826e924ca0918df
raihanbimaac <raihanbimaac@gmail.com>
2020-10-31 14:44:30 +0700

    Create praktikum4.py

---
+++ praktikum4.py
@@ -0,0 +1,17 @@
+#nomer1
+def jumlah(a,b):
+    global hasil
+    hasil=a + b
+    
+a=10
+b=20
+jumlah(a,b)
+print(hasil)
+#nomer5
+def myFunction():
+    a=20
+    print(a)
+
+a=10
+myFunction()
+print(a)
